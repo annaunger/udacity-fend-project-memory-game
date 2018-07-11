ef2b89b8227d3a08636514551a87764ff7ef9713
annaunger <anna.unger@hyperisland.se>
2018-07-11 14:02:22 +0200

    fix
    fix a bug

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
+ createNewCards();
  const deck = document.getElementById('totalDeck');
  let openArray = [];
- let shuffledArray = [];
  let matchedArray = [];
 
  const matchingCardsArray = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-cube', 'fa fa-cube'];
