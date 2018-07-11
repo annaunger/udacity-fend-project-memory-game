66bfb4918be2f83fb8b7e73cc86220f602de9eae
annaunger <anna.unger@hyperisland.se>
2018-07-11 13:32:41 +0200

    fix
    a bug fix, matchingCardsArray

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@
  * Create a list that holds all of your cards
  */
  const deck = document.getElementById('totalDeck');
- const matchingCardsArray = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+ const matchingCardsArray = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-cube', 'fa fa-cube'];
 /*
  * Create new cards
  */
+ let shuffledArray = [];
  function createNewCards() {
      shuffledArray = shuffle(matchingCardsArray);
      for (var i = 0; i < matchingCardsArray.length; i++) {
