e4acb98cd70bd0f82cab809bb228706f86fbe14a
annaunger <anna.unger@hyperisland.se>
2018-07-12 15:24:19 +0200

    style
    indention

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
+/*
+ * Create a list that holds all of your cards
+ */
+const icons = [
+    'fa fa-diamond',
+    'fa fa-diamond',
+    'fa fa-anchor',
+    'fa fa-anchor',
+    'fa fa-paper-plane-o',
+    'fa fa-paper-plane-o',
+    'fa fa-bolt',
+    'fa fa-bolt',
+    'fa fa-bomb',
+    'fa fa-bomb',
+    'fa fa-leaf',
+    'fa fa-leaf',
+    'fa fa-bicycle',
+    'fa fa-bicycle',
+    'fa fa-cube',
+    'fa fa-cube'
+];
 
-   /*
-    * Create a list that holds all of your cards
-    */
-    const icons = [
-      'fa fa-diamond',
-      'fa fa-diamond',
-      'fa fa-anchor',
-      'fa fa-anchor',
-      'fa fa-paper-plane-o',
-      'fa fa-paper-plane-o',
-      'fa fa-bolt',
-      'fa fa-bolt',
-      'fa fa-bomb',
-      'fa fa-bomb',
-      'fa fa-leaf',
-      'fa fa-leaf',
-      'fa fa-bicycle',
-      'fa fa-bicycle',
-      'fa fa-cube',
-      'fa fa-cube'];
+const cardsContainer = document.querySelector(".deck");
 
-  const cardsContainer = document.querySelector(".deck");
+let openedCards = [];
+let matchedCards = [];
 
-  let openedCards = [];
-  let matchedCards = [];
-
-  // Create the cards
-  for(let i = 0; i < icons.length; i++) {
+// Create the cards
+for (let i = 0; i < icons.length; i++) {
     const card = document.createElement("li");
     card.classList.add("card");
     card.innerHTML = `<i class="${icons[i]}"></i>`;
@@ -35,91 +35,86 @@
     // card click event
     card.addEventListener("click", function() {
 
-      const currentCard = this;
-      const previousCard = openedCards[0];
+        const currentCard = this;
+        const previousCard = openedCards[0];
 
-      // We have an existing OPENED card
-      if(openedCards.length === 1) {
+        // We have an existing OPENED card
+        if (openedCards.length === 1) {
 
+            card.classList.add("open", "show");
+            openedCards.push(this);
 
-        card.classList.add("open", "show");
-        openedCards.push(this);
+            // We should compare our 2 opened cards!
+            if (currentCard.innerHTML === previousCard.innerHTML) {
 
-        // We should compare our 2 opened cards!
-        if(currentCard.innerHTML === previousCard.innerHTML) {
+                // Matched
+                currentCard.classList.add("match");
+                previousCard.classList.add("match");
 
-          // Matched
-          currentCard.classList.add("match");
-          previousCard.classList.add("match");
+                matchedCards.push(currentCard, previousCard);
 
-          matchedCards.push(currentCard, previousCard);
+                openedCards = [];
 
-          openedCards = [];
+                // Check if the game is over!
+                isOver();
 
-          // Check if the game is over!
-          isOver();
+            } else {
+
+                // Wait 500 ms, then do this:
+                setTimeout(function() {
+                    currentCard.classList.remove("open", "show");
+                    previousCard.classList.remove("open", "show");
+                    openedCards = [];
+                }, 500);
+            }
 
         } else {
-
-
-          // Wait 500 ms, then do this:
-          setTimeout(function() {
-            currentCard.classList.remove("open", "show");
-            previousCard.classList.remove("open", "show");
-            openedCards = [];
-          }, 500);
-
-
-
+            // We don't have any opened cards
+            currentCard.classList.add("open", "show");
+            openedCards.push(this);
         }
-
-      } else {
-      // We don't have any opened cards
-        currentCard.classList.add("open", "show");
-        openedCards.push(this);
-      }
-
     });
-  }
+}
 
-  function isOver() {
-    if(matchedCards.length === icons.length) {
-      alert("Well done, you made it!")
+function isOver() {
+    if (matchedCards.length === icons.length) {
+        alert("Well done, you made it!")
     }
 
-  }
+}
 
 
-   /*if(openedCards.length === i)
-    * Display the cards on the page
-    *   - shuffle the list of cards using the provided "shuffle" method below
-    *   - loop through each card and create its HTML
-    *   - add each card's HTML to the page
-    */
+/*if(openedCards.length === i)
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
 
-   // Shuffle function from http://stackoverflow.com/a/2450976
-   function shuffle(array) {
-       var currentIndex = array.length, temporaryValue, randomIndex;
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-       while (currentIndex !== 0) {
-           randomIndex = Math.floor(Math.random() * currentIndex);
-           currentIndex -= 1;
-           temporaryValue = array[currentIndex];
-           array[currentIndex] = array[randomIndex];
-           array[randomIndex] = temporaryValue;
-       }
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-       return array;
-   }
+    return array;
+}
 
 
-   /*
-    * set up the event listener for a card. If a card is clicked:
-    *  - display the card's symbol (put this functionality in another function that you call from this one)
-    *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-    *  - if the list already has another card, check to see if the two cards match
-    *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-    *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-    *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-    *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-    */
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
